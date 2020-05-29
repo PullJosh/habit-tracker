97d05feae5a2f818b3ff8395b161133226e07f3b
Josh Pullen <JoshPullen27@gmail.com>
2020-05-29 15:43:48 -0400

    Add curve to success animation

--- components/SuccessScreen.js
+++ components/SuccessScreen.js
@@ -2,26 +2,31 @@ import { html, useState, useEffect } from "../htm-preact.js";
 
 export function SuccessScreen({ succeeded, undo }) {
   // Don't shake if you load the page and have already succeeded
-  const [allowShakeAnimation, setAllowShakeAnimation] = useState(!succeeded);
+  const [allowAnimation, setAllowAnimation] = useState(!succeeded);
 
   useEffect(() => {
     if (!succeeded) {
-      setAllowShakeAnimation(true);
+      setAllowAnimation(true);
     }
   }, [succeeded]);
 
   return html`
     <div
-      class=${"SuccessScreen" + (succeeded ? " SuccessScreen--visible" : "")}
+      class=${"SuccessScreen" +
+      (succeeded ? " SuccessScreen--visible" : "") +
+      (allowAnimation ? " SuccessScreen--allowAnimation" : "")}
       style=${{
         transform: `translateY(${succeeded ? "0%" : "100%"})`,
         transformOrigin: "bottom left",
         opacity: succeeded ? 1 : 0,
         transition: succeeded
-          ? "opacity 100ms ease-in, transform 400ms cubic-bezier(0.700, 0.000, 0.700, 0.000)"
+          ? "transform 400ms cubic-bezier(0.700, 0.000, 0.700, 0.000)"
           : "opacity 150ms ease-in-out, transform 150ms step-end",
       }}
     >
+      <svg viewBox="0 0 100 10" class="SuccessScreen__curve">
+        <path fill="black" d="M0 0 Q 50 20, 100 0" />
+      </svg>
       <div
         class="SuccessScreen__inner"
         style=${{
@@ -32,12 +37,7 @@ export function SuccessScreen({ succeeded, undo }) {
             : "none",
         }}
       >
-        <div
-          class=${"SuccessScreen__content" +
-          (allowShakeAnimation
-            ? " SuccessScreen__content--allowShakeAnimation"
-            : "")}
-        >
+        <div class="SuccessScreen__content">
           <h1 class="SuccessScreen__header">Good work. 🎉</h1>
           <button
             class="SuccessScreen__undoBtn"
